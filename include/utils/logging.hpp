92f110a5ddf01a5722aad4f284494294d3261205
agares <ross.kidson@gmail.com>
2020-12-28 16:23:54 +0100

    Added logging namespace.

--- include/utils/logging.hpp
+++ include/utils/logging.hpp
@@ -2,16 +2,18 @@
 
 #include <iostream>
 
+namespace logging {
 inline std::string getFilename(const std::string& str) {
   const auto found = str.find_last_of("/\\");
   return str.substr(found + 1);
 }
+}  // namespace logging
 
-#define LOG_ERROR(msg)                                                                            \
-  std::cerr << "[ERROR] " << getFilename(std::string{__FILE__}) << ":" << __LINE__ << ": " << msg \
-            << std::endl;
+#define LOG_ERROR(msg)                                                                      \
+  std::cerr << "[ERROR] " << logging::getFilename(std::string{__FILE__}) << ":" << __LINE__ \
+            << ": " << msg << std::endl;
 
-#define LOG_INFO(msg)                                                                            \
-  std::cout << "[INFO] " << getFilename(std::string{__FILE__}) << ":" << __LINE__ << ": " << msg \
-            << std::endl;
+#define LOG_INFO(msg)                                                                              \
+  std::cout << "[INFO] " << logging::getFilename(std::string{__FILE__}) << ":" << __LINE__ << ": " \
+            << msg << std::endl;
 
